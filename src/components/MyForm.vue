f5f502e728f6c65d35517c282b6835b595114b8a
Yangkailiang1 <yangkailiang163@outlook.com>
2025-12-15 09:27:24 +0800

    组件ref

---
+++ src/components/MyForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="formbox">
+        账号：<input type="text"/>
+        <br/>
+        密码：<input type="password"/>
+    </div>
+</template>
+
+<script setup>
+    const validate=()=>{
+        return Math.random()>0.5 ?true:false
+    }
+    //组件对外暴露
+    defineExpose({
+        validate
+    })
+</script>
+
+<style scoped>
+
+</style>
